e366cfe4eda546a11e8819c52ed278a118e2f05a
scarlat <scarlat.cristian.gabriel@gmail.com>
2026-01-04 00:07:58 +0200

    added jupyter

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-import pyspark 
+import pyspark
 from pyspark.sql import SparkSession
 
+# Create a Spark session (Control center of Spark functionality)
 spark = SparkSession.builder.appName("MySparkApp").getOrCreate()
 
 spark.range(10).explain()
 spark.range(10).show()
 
+df = spark.read.csv(
+    "201508_trip_data.csv",
+    header=True,
+    inferSchema=True
+)
+
+df.show(5)
+
+
+print(spark.conf.get("spark.app.name"))
+print(spark.conf.get("spark.sql.shuffle.partitions"))
+print(spark.conf.get("spark.sql.files.maxPartitionBytes"))
+print(spark.version)
+
+# Stop the Spark session - good practice to free up resources on low-RAM machines
+spark.stop()
